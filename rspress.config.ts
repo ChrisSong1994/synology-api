f71d0e57aade2a1b4509d6d10ac199a8259b156a
ChrisSong1994 <18557511748@163.com>
2025-07-15 19:45:20 +0800

    chore: deploy base path

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,8 +1,11 @@
 import * as path from "node:path";
 import { defineConfig } from "rspress/config";
 
+const BASE_PATH = process.env.BASE_PATH ? process.env.BASE_PATH : "/";
+
 export default defineConfig({
   root: path.join(__dirname, "document/docs"),
+  base: BASE_PATH,
   title: "Synology Api",
   themeConfig: {
     socialLinks: [
