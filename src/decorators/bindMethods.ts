beee0c513100ffbeef3ac903fabf06532d9d7a5c
ChrisSong1994 <18557511748@163.com>
2025-07-22 20:13:27 +0800

    chore: rebuild module methods bound to instance

--- src/decorators/bindMethods.ts
+++ src/decorators/bindMethods.ts
@@ -1,4 +1,4 @@
-// export type Method = (...args: any[]) => Promise<any>;
+export type Method = (...args: any[]) => Promise<any>;
 
 // type MethodKeys<T> = {
 //   [K in keyof T]: {
@@ -6,47 +6,40 @@
 //   };
 // }[keyof T];
 
-// /**
-//  * 将源对象的方法绑定到目标类的原型上，并确保方法的 this 指向类实例
-//  * @param source 包含要绑定方法的对象
-//  */
-// export function BindMethods<T extends object>(source: T): ClassDecorator {
-//   return function (target: { prototype: any }) {
-//     // 获取源对象的所有方法键
-//     const methodKeys = Object.getOwnPropertyNames(source).filter(
-//       (key): key is MethodKeys<T> => typeof source[key] === "function"
-//     );
+// bind methods to BaseSynologyApi instance
+function methodsBundler(instance: any, methods: any) {
+  const output = {};
+  for (const key in methods) {
+    output[key] = methods[key].bind(instance);
+  }
+  return output;
+}
 
-//     // 为每个方法创建绑定版本并定义到原型上
-//     methodKeys.forEach((methodName) => {
-//       const originalMethod = source[methodName] as Function;
+/**
+ * 将源对象的方法绑定到目标类的原型上，并确保方法的 this 指向类实例
+ * @param source 包含要绑定方法的对象
+ */
+export function BindMethods<T extends object>(source: T): ClassDecorator {
+  return function (target: { prototype: any }) {
+    // 获取源对象的所有方法键
+    const modulesKeys = Object.getOwnPropertyNames(source);
 
-//       // 创建绑定方法的描述符
-//       const descriptor: PropertyDescriptor = {
-//         configurable: true,
-//         enumerable: false, // 使方法不可枚举
+    // 为每个方法创建绑定版本并定义到原型上
+    modulesKeys.forEach((moduleName) => {
+      const methods = source[moduleName];
+      // 创建绑定方法的描述符
+      const descriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false, 
+        get() {
+          return methodsBundler(this, methods);
+        },
+        set(  ) {
+        //  this[moduleName] = value;
+        },
+      };
 
-//         // 使用 getter 确保每个实例都有自己的绑定函数
-//         get() {
-//           // 跳过原型访问，直接返回原始方法
-//           if (this === target.prototype || this === undefined) {
-//             return originalMethod;
-//           }
-
-//           // 为实例创建绑定方法并缓存
-//           const boundMethod = originalMethod.bind(this);
-//           Object.defineProperty(this, methodName, {
-//             value: boundMethod,
-//             configurable: true,
-//             writable: true,
-//             enumerable: false,
-//           });
-//           return boundMethod;
-//         },
-//       };
-
-//       // 将绑定方法定义到类原型上
-//       Object.defineProperty(target.prototype, methodName, descriptor);
-//     });
-//   };
-// }
+      Object.defineProperty(target.prototype, moduleName, descriptor);
+    });
+  };
+}
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,6 +2,7 @@ import * as AudioStation from "./AudioStation";
 import * as FileStation from "./FileStation";
 import * as VideoStation from "./VideoStation";
 import * as Auth from "./Auth";
+import { BindMethods } from "../decorators/bindMethods";
 
 export const SynologyApiKeys = {
   FileStation: FileStation.SPELLING_KEY,
@@ -20,8 +21,17 @@ export type SynologyApiMethods =
   | VideoStation.TMethods
   | Auth.TMethods;
 
-// export type BaseSynologyApiKeyMethods = FileStation.IKeyMethods
+export const SynologyApiMethods = {
+  [AudioStation.SPELLING_KEY]: AudioStation.METHODS,
+  [AudioStation.SIMPLIFY_KEY]: AudioStation.METHODS,
+  [FileStation.SPELLING_KEY]: FileStation.METHODS,
+  [FileStation.SIMPLIFY_KEY]: FileStation.METHODS,
+  [VideoStation.SPELLING_KEY]: VideoStation.METHODS,
+  [VideoStation.SIMPLIFY_KEY]: VideoStation.METHODS,
+  [Auth.SPELLING_KEY]: Auth.METHODS,
+};
 
+@BindMethods(SynologyApiMethods)
 export class BaseSynologyApi {
   [AudioStation.SPELLING_KEY]: AudioStation.TMethods;
   [AudioStation.SIMPLIFY_KEY]: AudioStation.TMethods;
@@ -35,58 +45,58 @@ export class BaseSynologyApi {
 }
 
 // bind methods to BaseSynologyApi instance
-function methodsBundler(instance: BaseSynologyApi, methods: SynologyApiMethods) {
-  const output = {};
-  for (const key in methods) {
-    output[key] = methods[key].bind(instance);
-  }
-  return output;
-}
+// function methodsBundler(instance: BaseSynologyApi, methods: SynologyApiMethods) {
+//   const output = {};
+//   for (const key in methods) {
+//     output[key] = methods[key].bind(instance);
+//   }
+//   return output;
+// }
 
-// proxy methods namespace to BaseSynologyApi instance
-Object.defineProperties(BaseSynologyApi.prototype, {
-  // FileStation
-  [SynologyApiKeys.FileStation]: {
-    get() {
-      return methodsBundler(this, FileStation.METHODS);
-    },
-  },
-  [SynologyApiKeys.fs]: {
-    get() {
-      return methodsBundler(this, FileStation.METHODS);
-    },
-  },
-  // AudioStation
-  [SynologyApiKeys.AudioStation]: {
-    get() {
-      return methodsBundler(this, AudioStation.METHODS);
-    },
-  },
-  [SynologyApiKeys.as]: {
-    get() {
-      return methodsBundler(this, AudioStation.METHODS);
-    },
-  },
-  // VideoStation
-  [SynologyApiKeys.VideoStation]: {
-    get() {
-      return methodsBundler(this, VideoStation.METHODS);
-    },
-  },
-  [SynologyApiKeys.vs]: {
-    get() {
-      return methodsBundler(this, VideoStation.METHODS);
-    },
-  },
-  // Auth
-  [SynologyApiKeys.Auth]: {
-    get() {
-      return methodsBundler(this, Auth.METHODS);
-    },
-  },
-  [SynologyApiKeys.au]: {
-    get() {
-      return methodsBundler(this, Auth.METHODS);
-    },
-  },
-});
+// // proxy methods namespace to BaseSynologyApi instance
+// Object.defineProperties(BaseSynologyApi.prototype, {
+//   // FileStation
+//   [SynologyApiKeys.FileStation]: {
+//     get() {
+//       return methodsBundler(this, FileStation.METHODS);
+//     },
+//   },
+//   [SynologyApiKeys.fs]: {
+//     get() {
+//       return methodsBundler(this, FileStation.METHODS);
+//     },
+//   },
+//   // AudioStation
+//   [SynologyApiKeys.AudioStation]: {
+//     get() {
+//       return methodsBundler(this, AudioStation.METHODS);
+//     },
+//   },
+//   [SynologyApiKeys.as]: {
+//     get() {
+//       return methodsBundler(this, AudioStation.METHODS);
+//     },
+//   },
+//   // VideoStation
+//   [SynologyApiKeys.VideoStation]: {
+//     get() {
+//       return methodsBundler(this, VideoStation.METHODS);
+//     },
+//   },
+//   [SynologyApiKeys.vs]: {
+//     get() {
+//       return methodsBundler(this, VideoStation.METHODS);
+//     },
+//   },
+//   // Auth
+//   [SynologyApiKeys.Auth]: {
+//     get() {
+//       return methodsBundler(this, Auth.METHODS);
+//     },
+//   },
+//   [SynologyApiKeys.au]: {
+//     get() {
+//       return methodsBundler(this, Auth.METHODS);
+//     },
+//   },
+// });
